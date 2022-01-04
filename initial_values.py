aee0b0aaafcb792a2a44c6da49ba4e84b71e82c8
Zhenya1975 <zhenya1975@mail.ru>
2022-01-04 12:08:40 +0300

    tab calendar actions prepared

--- initial_values.py
+++ initial_values.py
@@ -38,8 +38,8 @@ def get_curent_quarter_and_year():
     """получение текущего квартала и года"""
     current_date = datetime.datetime.now()
     """текущая дата в формате datetime"""
-
-    current_quarter = round((current_date.month - 1) / 3)
+    current_quarter = current_date.month//4 + 1
+    print(current_quarter)
     """номер текущего квартала"""
 
     current_year = current_date.year
